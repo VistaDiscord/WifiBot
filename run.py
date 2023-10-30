d3dc8ffea24aa739d96a8584c5babb524944cd0c
pikachubolk <noabolk@schoolsquid.xyz>
2023-10-30 10:22:51 +0100

    Commit

--- run.py
+++ run.py
@@ -1,58 +1,70 @@
 import discord
 import speedtest
 import asyncio
+import aiohttp
 
-# Your bot token goes here
 TOKEN = 'TOKEN'
-# Your channel ID goes here
-CHANNEL_ID = your-discord-channel-id
-# Your ping threshold in milliseconds
+CHANNEL_ID = 1161261373628620811
 PING_THRESHOLD = 50
-# Your speed thresholds in Mbps
-SPEED_THRESHOLD = 10
+SPEED_THRESHOLD = 15
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
+blocked = False  # global flag to indicate a 403 error
+
 async def check_speed():
-    st = speedtest.Speedtest()
-    st.get_best_server()
-    ping = st.results.ping
-    # Convert speeds from bits per second to megabits per second
-    download_speed = st.download() / 10**6
-    upload_speed = st.upload() / 10**6
-    return ping, download_speed, upload_speed
+    global blocked  # ensure we're updating the global variable
+    try:
+        loop = asyncio.get_running_loop()
+        st = speedtest.Speedtest()
+
+        # Wrap the synchronous parts in a thread executor
+        await loop.run_in_executor(None, st.get_best_server)
+        ping = st.results.ping
+        download_speed = await loop.run_in_executor(None, st.download) / (10**6)
+        upload_speed = await loop.run_in_executor(None, st.upload) / (10**6)
+
+        return ping, download_speed, upload_speed
+    except aiohttp.ClientResponseError as e:
+        if e.status == 403:
+            blocked = True
+            return None, None, None  # return None values if a 403 error occurs
+        raise  # re-raise the exception if it's not a 403 error
 
 async def send_status(channel_id):
     channel = client.get_channel(channel_id)
     while True:
-        ping, download_speed, upload_speed = await check_speed()
-        if ping > PING_THRESHOLD or download_speed <= SPEED_THRESHOLD or upload_speed <= SPEED_THRESHOLD:
-            embed = discord.Embed(
-                title="Network Status: Poor",
-                description=(
-                    "The Internet is bad, if you're watching videos, gaming or doing "
-                    "anything non-school related please stop.\n"
-                    f"Ping: {ping} ms\n"
-                    f"Download Speed: {download_speed:.2f} Mbps\n"
-                    f"Upload Speed: {upload_speed:.2f} Mbps"
-                ),
-                color=discord.Color.red()
-            )
+        if blocked:
+            await channel.send("Script is blocked from speedtest.net")
         else:
-            embed = discord.Embed(
-                title="Network Status: Good",
-                description=(
-                    f"The internet is Fine.\n"
-                    f"Ping: {ping} ms\n"
-                    f"Download Speed: {download_speed:.2f} Mbps\n"
-                    f"Upload Speed: {upload_speed:.2f} Mbps"
-                ),
-                color=discord.Color.green()
-            )
-
-        await channel.send(embed=embed)
-        await asyncio.sleep(1)  # Sleep for 5 minutes
+            ping, download_speed, upload_speed = await check_speed()
+            if ping is not None and download_speed is not None and upload_speed is not None:
+                if ping > PING_THRESHOLD or download_speed <= SPEED_THRESHOLD or upload_speed <= SPEED_THRESHOLD:
+                    embed = discord.Embed(
+                        title="Netwerkstatus: Slecht",
+                        description=(
+                            "Het internet is slecht. Als je video's kijkt, aan het gamen bent of "
+                            "iets doet dat niet schoolgerelateerd is, stop dan alsjeblieft.\n"
+                            f"Ping: {ping} ms\n"
+                            f"Download Speed: {download_speed:.2f} Mbps\n"
+                            f"Upload Speed: {upload_speed:.2f} Mbps"
+                        ),
+                        color=discord.Color.red()
+                    )
+                else:
+                    embed = discord.Embed(
+                        title="Netwerkstatus: Goed",
+                        description=(
+                            f"Het internet is prima.\n"
+                            f"Ping: {ping} ms\n"
+                            f"Download Speed: {download_speed:.2f} Mbps\n"
+                            f"Upload Speed: {upload_speed:.2f} Mbps"
+                        ),
+                        color=discord.Color.green()
+                    )
+                await channel.send(embed=embed)
+        await asyncio.sleep(600)  # Sleep for 10 minutes
 
 @client.event
 async def on_ready():
